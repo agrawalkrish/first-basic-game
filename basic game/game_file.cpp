db0e746ba5a93a9acc48b653372f434be73bf87b
Krishna <agrawalkrish958@gmail.com>
2025-05-29 10:40:37 +0530

    idk just did something and something works

--- basic game/game_file.cpp
+++ basic game/game_file.cpp
@@ -11,9 +11,9 @@ internal void
 simulate_game(Input* input, float dt) {
 
     // Clears the screen with a solid background color (hex: 0xff5500, a shade of orange)  
-    clear_screen(0x000000);
+    clear_screen(0xff5500);
 
-
+    draw_rect(0, 0, 85, 45, 0xffaa33);
     float speed = 50.f;
     // Check if the "Up" button is pressed  
     if (is_down(BUTTON_UP)) player_pos_y += speed * dt;
@@ -21,9 +21,10 @@ simulate_game(Input* input, float dt) {
     if (is_down(BUTTON_RIGHT)) player_pos_x += speed * dt;
     if (is_down(BUTTON_LEFT)) player_pos_x -= speed * dt;
         // Draws a small rectangle at the center when the "Up" button is pressed  
-        draw_rect(player_pos_x, player_pos_y, 1, 1, 0xaa7348);
+    //draw_rect(player_pos_x, player_pos_y, 1, 1, 0xaa7348);
 
-    // Draws additional rectangles regardless of input  
-    draw_rect(30, 30, 5, 5, 0xaac348); // A rectangle at (30,30) with size (5,5)  
-    draw_rect(-20, 20, 8, 3, 0xaac348); // A rectangle at (-20,20) with size (8,3)  
+    // Draws additional rectangles regardless of input
+    draw_rect(0, 0, 1, 1, 0xffffff);
+    draw_rect(80, player_pos_y, 2.5, 12, 0xff0000); // A rectangle at (30,30) with size (5,5)  
+    draw_rect(-80, 0, 2.5, 12, 0xff0000); // A rectangle at (-20,20) with size (8,3)  
 }
--- basic game/win32_platform.cpp
+++ basic game/win32_platform.cpp
@@ -127,8 +127,8 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 #define process_button(b,vk)\
 case vk: {\
+input.buttons[b].changed = is_down!=input.buttons[b].is_down;\
 input.buttons[b].is_down = is_down;\
-input.buttons[b].changed = true;\
 }break;\
                 // Handle specific key press inputs  
                 switch (vk_code) {
