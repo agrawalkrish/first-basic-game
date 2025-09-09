41cd20330130f7942a533beb96559379ba90a3ef
agrawalkrish <agrawalkrish958@gmail.com>
2025-09-09 17:39:57 +0530

    added ball physics

--- basic game/game_file.cpp
+++ basic game/game_file.cpp
@@ -1,68 +1,114 @@
-#define is_down(b) input->buttons[b].is_down                                  // Button held
+#define is_down(b) input->buttons[b].is_down                           // Button held
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)   // Just pressed
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed) // Just released
 
+// Player variables
 float player_1_p, player_1_dp;
 float player_2_p, player_2_dp;
-float arean_half_size_x = 85, arean_half_size_y = 45;
+float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 
-float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y;
+// Ball variables - Initialized for a consistent start
+float ball_p_x = 0, ball_p_y = 0;
+float ball_dp_x = 130, ball_dp_y = 0;
+float ball_half_size = 1;
 
-internal void simulate_game(Input *input, float dt)
+
+internal void simulate_game(Input* input, float dt)
 {
-    clear_screen(0xff5500);                                          // Orange background
-    draw_rect(0, 0, arean_half_size_x, arean_half_size_y, 0xffaa33); // Main playfield
+    clear_screen(0xff5500);                                       // Orange background
+    draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33); // Main playfield
 
+    // Player 1 input and physics
     float player_1_ddp = 0;
-    float player_2_ddp = 0;
-
-    if (is_down(BUTTON_UP))
-        player_1_ddp += 2000; // P1 move up
-    if (is_down(BUTTON_DOWN))
-        player_1_ddp -= 2000; // P1 move down
-
-    if (is_down(BUTTON_W))
-        player_2_ddp += 2000; // P2 move up
-    if (is_down(BUTTON_S))
-        player_2_ddp -= 2000; // P2 move down
-
+    if (is_down(BUTTON_UP)) player_1_ddp += 2000;
+    if (is_down(BUTTON_DOWN)) player_1_ddp -= 2000;
     player_1_ddp -= player_1_dp * 10.f; // P1 friction
     player_1_p += player_1_dp * dt + 0.5f * player_1_ddp * dt * dt;
     player_1_dp += player_1_ddp * dt;
 
-    // p1 collision
-    if (player_1_p + player_half_size_y > arean_half_size_y)
-    {
-        player_1_p = arean_half_size_y - player_half_size_y;
-        player_1_dp *= 0;//change to 0 for no bounce NOTE
+    // Player 1 wall collision
+    if (player_1_p + player_half_size_y > arena_half_size_y) {
+        player_1_p = arena_half_size_y - player_half_size_y;
+        player_1_dp *= 0;
     }
-    else if (player_1_p - player_half_size_y < -arean_half_size_y)
-    {
-        player_1_p = -arean_half_size_y + player_half_size_y;
-        player_1_dp *= 0;//change to 0 for no bounce NOTE
+    else if (player_1_p - player_half_size_y < -arena_half_size_y) {
+        player_1_p = -arena_half_size_y + player_half_size_y;
+        player_1_dp *= 0;
     }
 
+    // Player 2 input and physics
+    float player_2_ddp = 0;
+    if (is_down(BUTTON_W)) player_2_ddp += 2000;
+    if (is_down(BUTTON_S)) player_2_ddp -= 2000;
     player_2_ddp -= player_2_dp * 10.f; // P2 friction
     player_2_p += player_2_dp * dt + 0.5f * player_2_ddp * dt * dt;
     player_2_dp += player_2_ddp * dt;
 
-    // p2 collision
-    if (player_2_p + player_half_size_y > arean_half_size_y)
-    {
-        player_2_p = arean_half_size_y - player_half_size_y;
-        player_2_dp *= 0;//change to 0 for no bounce NOTE
+    // Player 2 wall collision
+    if (player_2_p + player_half_size_y > arena_half_size_y) {
+        player_2_p = arena_half_size_y - player_half_size_y;
+        player_2_dp *= 0;
     }
-    else if (player_2_p - player_half_size_y < -arean_half_size_y)
-    {
-        player_2_p = -arean_half_size_y + player_half_size_y;
-        player_2_dp *= 0;//change to 0 for no bounce NOTE
+    else if (player_2_p - player_half_size_y < -arena_half_size_y) {
+        player_2_p = -arena_half_size_y + player_half_size_y;
+        player_2_dp *= 0;
     }
 
+    // Ball movement
     ball_p_x += ball_dp_x * dt;
     ball_p_y += ball_dp_y * dt;
 
-    draw_rect(ball_p_x, ball_p_y, 1, 1, 0xffffff);// ball
+    // Ball collision detection and response
+    // Player 1 (Right Paddle) collision check
+    if (ball_p_x + ball_half_size > 80 - player_half_size_x &&
+        ball_p_x - ball_half_size < 80 + player_half_size_x &&
+        ball_p_y + ball_half_size > player_1_p - player_half_size_y &&
+        ball_p_y - ball_half_size < player_1_p + player_half_size_y)
+    {
+        ball_p_x = 80 - player_half_size_x - ball_half_size;
+        ball_dp_x *= -1;
+        ball_dp_y = (ball_p_y - player_1_p) * 2; // Add vertical speed based on impact point
+    }
+    // Player 2 (Left Paddle) collision check
+    else if (ball_p_x - ball_half_size < -80 + player_half_size_x &&
+        ball_p_x + ball_half_size > -80 - player_half_size_x &&
+        ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
+        ball_p_y - ball_half_size < player_2_p + player_half_size_y)
+    {
+        ball_p_x = -80 + player_half_size_x + ball_half_size;
+        ball_dp_x *= -1;
+        ball_dp_y = (ball_p_y - player_2_p) * 2; // Add vertical speed based on impact point
+    }
+
+    // Ball collision with top and bottom walls
+    if (ball_p_y + ball_half_size > arena_half_size_y) {
+        ball_p_y = arena_half_size_y - ball_half_size;
+        ball_dp_y *= -1;
+    }
+    else if (ball_p_y - ball_half_size < -arena_half_size_y) {
+        ball_p_y = -arena_half_size_y + ball_half_size;
+        ball_dp_y *= -1;
+    }
+
+    // Ball collision with side walls (scoring and reset)
+    if (ball_p_x + ball_half_size > arena_half_size_x) {
+        // Player 2 scores, reset ball
+        ball_p_x = 0;
+        ball_p_y = 0;
+        ball_dp_y = 0;
+        ball_dp_x *= -1; // Serve to the other player
+    }
+    else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+        // Player 1 scores, reset ball
+        ball_p_x = 0;
+        ball_p_y = 0;
+        ball_dp_y = 0;
+        ball_dp_x *= -1; // Serve to the other player
+    }
+
+    // Draw everything
+    draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // ball
     draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);  // Right paddle
     draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000); // Left paddle
 }
