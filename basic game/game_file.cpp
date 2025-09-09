46afdb01aed2cc42c1120315e53e2eb92dd1b2db
agrawalkrish <agrawalkrish958@gmail.com>
2025-09-09 17:14:43 +0530

    added ball

--- basic game/game_file.cpp
+++ basic game/game_file.cpp
@@ -6,6 +6,9 @@ float player_1_p, player_1_dp;
 float player_2_p, player_2_dp;
 float arean_half_size_x = 85, arean_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
+
+float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y;
+
 internal void simulate_game(Input *input, float dt)
 {
     clear_screen(0xff5500);                                          // Orange background
@@ -32,29 +35,34 @@ internal void simulate_game(Input *input, float dt)
     if (player_1_p + player_half_size_y > arean_half_size_y)
     {
         player_1_p = arean_half_size_y - player_half_size_y;
-        player_1_dp *= -1;//change to 0 for no bounce NOTE
+        player_1_dp *= 0;//change to 0 for no bounce NOTE
     }
-    else if (player_1_p - player_half_size_y < - arean_half_size_y)
+    else if (player_1_p - player_half_size_y < -arean_half_size_y)
     {
-        player_1_p = arean_half_size_y + player_half_size_y;
-        player_1_dp *= 1;//change to 0 for no bounce NOTE
+        player_1_p = -arean_half_size_y + player_half_size_y;
+        player_1_dp *= 0;//change to 0 for no bounce NOTE
     }
 
     player_2_ddp -= player_2_dp * 10.f; // P2 friction
     player_2_p += player_2_dp * dt + 0.5f * player_2_ddp * dt * dt;
     player_2_dp += player_2_ddp * dt;
+
     // p2 collision
     if (player_2_p + player_half_size_y > arean_half_size_y)
     {
         player_2_p = arean_half_size_y - player_half_size_y;
-        player_2_dp *= -1;//change to 0 for no bounce NOTE
+        player_2_dp *= 0;//change to 0 for no bounce NOTE
     }
-    else if (player_2_p - player_half_size_y < - arean_half_size_y)
+    else if (player_2_p - player_half_size_y < -arean_half_size_y)
     {
-        player_2_p = arean_half_size_y + player_half_size_y;
-        player_2_dp *= -1;//change to 0 for no bounce NOTE
+        player_2_p = -arean_half_size_y + player_half_size_y;
+        player_2_dp *= 0;//change to 0 for no bounce NOTE
     }
-    draw_rect(0, 0, 1, 1, 0xffffff);                                              // Center marker
+
+    ball_p_x += ball_dp_x * dt;
+    ball_p_y += ball_dp_y * dt;
+
+    draw_rect(ball_p_x, ball_p_y, 1, 1, 0xffffff);// ball
     draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000);  // Right paddle
     draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000); // Left paddle
 }
